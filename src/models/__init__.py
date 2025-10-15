bc79455f8d707e95450a24c5daf2db120dd7ca00
Darsua <darrelyanuar@gmail.com>
2025-10-15 14:36:17 +0700

    fix: day constriction

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,6 +1,6 @@
 from .student import Student
 from .course_class import CourseClass
 from .room import Room
-from .time_slot import TimeSlot, Day
+from .time_slot import TimeSlot
 
-__all__ = ['Student', 'CourseClass', 'Room', 'TimeSlot', 'Day']
+__all__ = ['Student', 'CourseClass', 'Room', 'TimeSlot']
